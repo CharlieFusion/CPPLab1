89933dbca5226695f13c251517b0d7762090ba47
chudo <incodnako@inbox.ru>
2026-03-26 11:45:18 +0300

    Fix sorter_tests

---
+++ tests/sorter_tests.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cassert>
+#include "../include/sorter_lib/QuickSorter.h"
+#include "../include/sorter_lib/CocktailSorter.h"
+
+
+void TestQuickSorter() {
+    std::cout << "Nothing here yet" << std::endl;
+}
+
+void TestCocktailSorter() {
+    std::cout << "Nothing here yet" << std::endl;
+}
+
+int main() {
+
+    TestQuickSorter();
+    TestCocktailSorter();
+    std::cout << "Tests passed" << std::endl;
+
+    return 0;
+}
